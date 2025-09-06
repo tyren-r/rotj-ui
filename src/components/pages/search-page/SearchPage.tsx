de96c4ecdec2318346c41dfe547875d8fd0b50e2
Tyren <tyrenrhinehart@gmail.com>
2025-09-06 01:10:38 -0600

    create subtitle, change title to pyswapi

--- src/components/pages/search-page/SearchPage.tsx
+++ src/components/pages/search-page/SearchPage.tsx
@@ -1,3 +1,4 @@
+
 import SearchBar from '../../search/search-bar/SearchBar';
 import SearchResultsTable from '../../search/search-results/ResultTable';
 import './search-page-styles.css';
@@ -5,7 +6,8 @@ import './search-page-styles.css';
 const SearchPage = () => {
     return (
         <div id="page">
-            <h1 id="title">SWAPI</h1>
+            <h1 id="title">PYSWAPI</h1>
+            <p id="subtitle">The Python Star Wars API</p>
             <SearchBar />
             <SearchResultsTable />
         </div>
