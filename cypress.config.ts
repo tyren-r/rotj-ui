4631ad1b5e74b9ca21805af17b1d9b985cf7af85
Tyren <tyrenrhinehart@gmail.com>
2025-09-19 05:51:34 -0600

    set up config for cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+            config.baseUrl = 'http://localhost:3000';
+            config.experimentalStudio = true;
+            return config;
+        },
+    },
+});
