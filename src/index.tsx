d98d81fec80e53e43288f36484bd50b42efaae3f
Tyren <tyrenrhinehart@gmail.com>
2025-09-26 03:39:03 -0600

    merge in feature/routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import AboutPage from './components/pages/about-page/AboutPage';
+import DocumentationPage from './components/pages/documentation-page/DocumentationPage';
+import SearchPage from './components/pages/search-page/SearchPage';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <div id="stars" />
+        <div id="stars2" />
+        <div id="stars3" />
+        <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/documentation" element={<DocumentationPage />} />
+        </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
