79ef0135e8b2ec28e5e3eae8f5c2c554804e044f
Tyren <tyrenrhinehart@gmail.com>
2025-09-26 03:39:03 -0600

    provide the searchresults context provider to searchpage

--- src/components/pages/search-page/SearchPage.tsx
+++ src/components/pages/search-page/SearchPage.tsx
@@ -1,15 +1,18 @@
+import SearchResultsContextProvider from '../../../SearchResultsContextProvider';
 import SearchBar from '../../search/search-bar/SearchBar';
 import SearchResultsTable from '../../search/search-results/ResultTable';
 import './search-page-styles.css';
 
 const SearchPage = () => {
     return (
-        <div id="page">
-            <h1 id="title">PYSWAPI</h1>
-            <p id="subtitle">The Python Star Wars API</p>
-            <SearchBar />
-            <SearchResultsTable />
-        </div>
+        <SearchResultsContextProvider>
+            <div id="page">
+                <h1 id="title">PYSWAPI</h1>
+                <p id="subtitle">The Python Star Wars API</p>
+                <SearchBar />
+                <SearchResultsTable />
+            </div>
+        </SearchResultsContextProvider>
     );
 };
 
