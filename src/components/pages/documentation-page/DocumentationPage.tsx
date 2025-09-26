63a0ad05ada5881e720c43365eea83409ba8b876
Tyren <tyrenrhinehart@gmail.com>
2025-09-26 04:18:24 -0600

    use the new swagger library and custom styles to create our documentation page

--- src/components/pages/documentation-page/DocumentationPage.tsx
+++ src/components/pages/documentation-page/DocumentationPage.tsx
@@ -1,2 +1,8 @@
-const DocumentationPage: React.FC = () => <h1>Documentation Page</h1>;
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css'; 
+import './documentation-page-styles.css';
+
+const DocumentationPage: React.FC = () => {
+    return <SwaggerUI url="http://localhost:8000/openapi.json" />;
+}
 export default DocumentationPage;
