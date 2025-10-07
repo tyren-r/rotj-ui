3708dc4de4fd0bbf025c6eec6cf2a231bffcf971
Tyren <tyrenrhinehart@gmail.com>
2025-10-06 18:43:53 -0600

    use mui cards for about sections

--- src/components/pages/about-page/AboutPage.tsx
+++ src/components/pages/about-page/AboutPage.tsx
@@ -1,4 +1,6 @@
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import "./about-page-styles.css";
 import aboutTextJson from "./about-page-text.json";
@@ -6,14 +8,16 @@ import aboutTextJson from "./about-page-text.json";
 const AboutPage: React.FC = () => (
   <Box id="about-page-container">
     {aboutTextJson.map((section) => (
-      <>
-        <Typography gutterBottom variant="h4" align="center">
-          {section.title}
+      <Card variant="outlined" className="about-section" key={section.title}>
+        <CardContent>
+          <Typography gutterBottom variant="h4" align="center">
+            {section.title}
+          </Typography>
+          <Typography gutterBottom variant="body1" paragraph>
+            {section.paragraph}{" "}
         </Typography>
-        <Typography gutterBottom variant="body1" paragraph>
-          {section.paragraph}{" "}
-        </Typography>
-      </>
+        </CardContent>
+      </Card>
     ))}
   </Box>
 );
