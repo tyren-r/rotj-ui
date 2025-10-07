b12c04d31a594f2054d10d9da3e8e864340ed561
Tyren <tyrenrhinehart@gmail.com>
2025-10-07 16:52:53 -0600

    merge in feature/footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import { AppBar, Toolbar, Typography, Link, Box } from "@mui/material";
+import "./footer-styles.css";
+
+const Footer = () => {
+  return (
+    <AppBar component="footer" id="footer-appbar">
+      <Toolbar id="footer-toolbar">
+        <Typography variant="caption" sx={{ flexGrow: 1 }}>
+          © {new Date().getFullYear()} Tyren Rhinehart
+        </Typography>
+        <Box className="footer-links">
+          <Link
+            href="https://www.linkedin.com/in/tr95/"
+            underline="hover"
+            target="_blank"
+            color="inherit"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href="https://github.com/"
+            underline="hover"
+            target="_blank"
+            rel="noopener"
+            color="inherit"
+          >
+            GitHub
+          </Link>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Footer;
