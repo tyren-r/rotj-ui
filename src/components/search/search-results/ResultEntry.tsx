c5090a519f70e4867f76eb06a0ef33d4ceb3c4e5
Tyren <tyrenrhinehart@gmail.com>
2025-10-06 21:14:05 -0600

    merge in bugFix/console-errors

--- src/components/search/search-results/ResultEntry.tsx
+++ src/components/search/search-results/ResultEntry.tsx
@@ -3,6 +3,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import type { ResultEntryProps } from "../../../../types";
@@ -20,32 +21,35 @@ const ResultEntry: React.FC<ResultEntryProps> = ({ resultEntry }) => {
   return (
     <Card variant="outlined" className="result-entry-card">
       <CardMedia className="result-entry-image" image={resultEntry.image_url} />
-      <CardContent>
+      <CardContent className="result-entry-content">
         <Typography gutterBottom variant="h5" component="div">
           {resultEntry.name}
         </Typography>
         <Typography gutterBottom variant="body2">
           {resultEntry.description}
         </Typography>
-        <Table>
-          {Object.entries(resultEntryWithoutCommonFields).map(
-            ([key, value]) => (
-              <TableRow>
-                <TableCell className="table-cell">
-                  <Typography variant="body2">
-                    {key.replaceAll("_", " ")}
-                  </Typography>
-                </TableCell>
-                <TableCell className="table-cell">
-                  <Typography variant="body2">
-                    {/* 
+        <Table id="inner-table">
+          <TableBody>
+            {Object.entries(resultEntryWithoutCommonFields).map(
+              ([key, value], i) => (
+                // Using index as key since these keys are not unique and order won't change
+                <TableRow key={i}>
+                  <TableCell className="table-cell">
+                    <Typography variant="body2">
+                      {key.replaceAll("_", " ")}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className="table-cell">
+                    <Typography variant="body2">
+                      {/* 
 // @ts-expect-error The value variable is a string, but unsure how to properly get ts to recognize TODO*/}
-                    {value}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            ),
-          )}
+                      {value}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ),
+            )}
+          </TableBody>
         </Table>
       </CardContent>
     </Card>
