187aff5a57a15e96364384ac9c7c8527d1684766
Tyren <tyrenrhinehart@gmail.com>
2025-10-07 17:54:20 -0600

    merge in feature/attributions

--- src/components/pages/about-page/AboutPage.tsx
+++ src/components/pages/about-page/AboutPage.tsx
@@ -19,6 +19,66 @@ const AboutPage: React.FC = () => (
         </CardContent>
       </Card>
     ))}
+    <Card variant="outlined" className="about-section">
+      <CardContent>
+        <Typography gutterBottom variant="h4" align="center">
+          Attributions
+        </Typography>
+        <Typography gutterBottom variant="body1" paragraph>
+          <a
+            target="_blank"
+            style={{ color: "inherit" }}
+            href="https://icons8.com/icon/QqhR3q5pMyCZ/jedi"
+          >
+            Jedi
+          </a>{" "}
+          icon by{" "}
+          <a
+            style={{ color: "inherit" }}
+            target="_blank"
+            href="https://icons8.com"
+          >
+            Icons8
+          </a>
+          <br />
+          <a
+            target="_blank"
+            style={{ color: "inherit" }}
+            href="https://icons8.com/icon/8113/menu"
+          >
+            Menu
+          </a>{" "}
+          icon by{" "}
+          <a
+            style={{ color: "inherit" }}
+            target="_blank"
+            href="https://icons8.com"
+          >
+            Icons8
+          </a>
+          <br />
+          Starry parallax background orignally from{" "}
+          <a
+            target="_blank"
+            style={{ color: "inherit" }}
+            href="https://codepen.io/sarazond/pen/LYGbwj"
+          >
+            sarazond CSS Starry Parallax
+          </a>
+          , modified for React.
+          <br />
+          Json Data has been heavily modified but originally from{" "}
+          <a
+            target="_blank"
+            style={{ color: "inherit" }}
+            href="https://github.com/semperry/swapi"
+          >
+            Original SWAPI Github Copy
+          </a>{" "}
+          but it looks like the json data may have been removed from the repo.
+        </Typography>
+      </CardContent>
+    </Card>
   </Box>
 );
 export default AboutPage;
