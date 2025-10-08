309f0ce602950b372b4c5130b68255164348f9db
Tyren <tyrenrhinehart@gmail.com>
2025-10-08 13:54:10 -0600

    add aria roles etc to DocumentationPage

--- src/components/pages/documentation-page/DocumentationPage.tsx
+++ src/components/pages/documentation-page/DocumentationPage.tsx
@@ -1,8 +1,13 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import "./documentation-page-styles.css";
+import Box from "@mui/material/Box";
 
 const DocumentationPage: React.FC = () => {
-  return <SwaggerUI url="http://localhost:8000/openapi.json" />;
+  return (
+    <Box role="main" aria-label="API documentation">
+      <SwaggerUI url="http://localhost:8000/openapi.json" />
+    </Box>
+  );
 };
 export default DocumentationPage;
