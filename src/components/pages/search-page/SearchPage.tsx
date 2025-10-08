898defad81f54350afb22d8cecffd7f0687aa680
Tyren <tyrenrhinehart@gmail.com>
2025-10-08 14:00:35 -0600

    add aria roles etc to SearchPage,SearchBar

--- src/components/pages/search-page/SearchPage.tsx
+++ src/components/pages/search-page/SearchPage.tsx
@@ -3,17 +3,18 @@ import SearchBar from "../../search/search-bar/SearchBar";
 import SearchResultsTable from "../../search/search-results/ResultTable";
 import "./search-page-styles.css";
 import LoadingSpinner from "../../search/loading-spinner/LoadingSpinner";
+import { Box } from "@mui/material";
 
 const SearchPage = () => {
   return (
     <SearchResultsContextProvider>
-      <div id="page">
+      <Box id="page" role="main" aria-labelledby="title">
         <h1 id="title">SWAPI: Return of the Jedi</h1>
         <p id="subtitle">The newest iteration of the Star Wars API</p>
         <SearchBar />
         <LoadingSpinner />
         <SearchResultsTable />
-      </div>
+      </Box>
     </SearchResultsContextProvider>
   );
 };
